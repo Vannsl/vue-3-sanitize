e587d0eda6ed7be8c45d1d8924a1e46baf029a85
MattAtESP <matt@etherealswordproductions.com>
2021-08-23 00:45:35 -0400

    rebuilt with index.js included in package.json

--- dist/vue-3-sanitize.js
+++ dist/vue-3-sanitize.js
@@ -1,22 +1,22 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['vue-3-sanitize'] = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['vue-3-sanitize'] = factory());
 }(this, (function () { 'use strict';
 
-  var sanitizeHtml = require("sanitize-html");
+    var sanitizeHtml = require("sanitize-html");
 
-  var index = {
-    install: function install(app, options) {
-      app.config.globalProperties.$sanitize = function (dirty) {
-        var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-        return sanitizeHtml(dirty, opts || options);
-      };
-    },
-    defaults: sanitizeHtml.defaults
-  };
+    var Vue3Sanitize = {
+      install: function install(app, options) {
+        app.config.globalProperties.$sanitize = function (dirty) {
+          var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+          return sanitizeHtml(dirty, opts || options);
+        };
+      },
+      defaults: sanitizeHtml.defaults
+    };
 
-  return index;
+    return Vue3Sanitize;
 
 })));
 //# sourceMappingURL=vue-3-sanitize.js.map
